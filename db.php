aab36ed0ce53be8d506af571ce12e2a1552e45b9
Rajat Bandhral <105308791+Rajat972@users.noreply.github.com>
2026-01-08 21:05:04 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "your-rds-endpoint.amazonaws.com";
-$user = "admin";
-$password = "yourpassword";
-$dbname = "contact_app";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("DB connection failed");
 }
 ?>
