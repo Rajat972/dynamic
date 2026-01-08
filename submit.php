c38b70bba1dae87487b607e847a7406207665894
Rajat Bandhral <105308791+Rajat972@users.noreply.github.com>
2026-01-08 21:05:51 +0530

    Update and rename save_contact.php to submit.php

---
+++ submit.php
@@ -0,0 +1,19 @@
+<?php
+include 'db.php';
+
+$name    = $_POST['name'];
+$email   = $_POST['email'];
+$phone   = $_POST['phone'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contacts (name,email,phone,message)
+        VALUES ('$name','$email','$phone','$message')";
+
+if ($conn->query($sql)) {
+    echo "Thank you! Data saved.";
+} else {
+    echo "Error";
+}
+
+$conn->close();
+?>
