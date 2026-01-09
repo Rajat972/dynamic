a7f68a7fe13877590204df7a96679d8627df7adb
Rajat Bandhral <105308791+Rajat972@users.noreply.github.com>
2026-01-09 23:45:43 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = "html-testing-db.cg3uoe4ga0f3.us-east-1.rds.amazonaws.com";
+$host = "freelancing-testing-db.cg3uoe4ga0f3.us-east-1.rds.amazonaws.com";
 $user = "admin";
 $pass = "Rajat0117";
 $db   = "contactdb";
@@ -7,6 +7,6 @@ $db   = "contactdb";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("DB connection failed");
+    die("DB connection failed: " . $conn->connect_error);
 }
 ?>
